3ca4d73a9b4eeb3d168b9f84cb8ac6d25d7c02a2
Maxim Volgin <maxim.volgin@me.com>
2019-02-23 08:28:38 +0100

    macOS target

---
+++ iOS_RxSpriteKit/iOS_RxSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  iOS_RxSpriteKit.h
+//  iOS_RxSpriteKit
+//
+//  Created by Maxim Volgin on 23/02/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS_RxSpriteKit.
+FOUNDATION_EXPORT double iOS_RxSpriteKitVersionNumber;
+
+//! Project version string for iOS_RxSpriteKit.
+FOUNDATION_EXPORT const unsigned char iOS_RxSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_RxSpriteKit/PublicHeader.h>
+
+
---
+++ macOS_RxSpriteKit/macOS_RxSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  macOS_RxSpriteKit.h
+//  macOS_RxSpriteKit
+//
+//  Created by Maxim Volgin on 23/02/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for macOS_RxSpriteKit.
+FOUNDATION_EXPORT double macOS_RxSpriteKitVersionNumber;
+
+//! Project version string for macOS_RxSpriteKit.
+FOUNDATION_EXPORT const unsigned char macOS_RxSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <macOS_RxSpriteKit/PublicHeader.h>
+
+

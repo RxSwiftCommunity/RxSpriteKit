83828ebe216e871513fda7e58cf43c988cfbd356
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 20:06:34 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+  
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "RxSpriteKit",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "RxSpriteKit",
+            targets: ["RxSpriteKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "RxSpriteKit",
+            dependencies: ["RxSwift"], // "RxRelay"],
+            path: "."),
+//        .testTarget(
+//            name: "RxSpriteKitTests",
+//            dependencies: ["RxSpriteKit"],
+//            path: "Tests"
+//        )
+    ]
+)
